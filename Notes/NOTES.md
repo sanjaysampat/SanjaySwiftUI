d15937a90bc018687106206683533e8598dea59c
Sanjay Sampat <sanjaysampat@hotmail.com>
2020-10-07 18:17:10 +0530

    Notes addition.

--- Notes/NOTES.md
+++ Notes/NOTES.md
@@ -1,12 +1,24 @@
 # Notes
 
-## Reference Examples
+## Additional Reference and Examples
 
 ### MVVM
 
 1. [Flip clock implementation in SwiftUI](https://github.com/elpassion/FlipClock-SwiftUI) by [EL Passion](https://www.elpassion.com/)
-
 ![Flip clock](Flipclock_file_structure.png)
+
+
+### Apple Pay
+
+1. [Guide by Apple](https://developer.apple.com/library/archive/ApplePay_Guide/)
+
+2. [List of Payment service providers](https://developer.apple.com/apple-pay/payment-platforms/)
+![Liist of Payment service providers by Apple as on Oct 7, 2020](apple-pay-payment-service-providers.jpg)
+
+3. [Enable Apple Pay by Square payment provider](https://developer.squareup.com/docs/in-app-payments-sdk/add-digital-wallets/apple-pay)
+
+4. [Apple Pay Tutorial @ raywenderlich.com](https://www.raywenderlich.com/2113-apple-pay-tutorial-getting-started)
+
 
 -- End --
 
--- SanjaySwiftUI/SpriteKitResources/EmitterSpriteKitView.swift
+++ SanjaySwiftUI/SpriteKitResources/EmitterSpriteKitView.swift
@@ -12,28 +12,28 @@ import SpriteKit
 struct EmitterSpriteKitView: UIViewRepresentable {
     
     /**
-        makeUIView and updateUIView are required methods of the UIViewRepresentable
-        protocol. makeUIView does just what you'd think - makes the view we want.
-
-        updateUIView allows you to update the view with new data, but we don't need
-        it for our purposes.
-        */
+     makeUIView and updateUIView are required methods of the UIViewRepresentable
+     protocol. makeUIView does just what you'd think - makes the view we want.
+     
+     updateUIView allows you to update the view with new data, but we don't need
+     it for our purposes.
+     */
     
     func makeUIView(context: UIViewRepresentableContext<EmitterSpriteKitView>) -> SKView {
         // Create our SKView
-                let view = SKView()
-                // We want the view to animate the particle effect over our SwiftUI view
-                // and let its components show through so we'll set allowsTransparenty to true.
-                view.allowsTransparency = true
-                // Load our custom SKScene
-                // More info about creating scene in Xcode is
-                // https://developer.apple.com/documentation/spritekit/skscene/creating_a_scene_from_a_file
-                if let scene = SceneSanjay(fileNamed: "MySceneSanjay.sks") {
-                    // We need to set the background to clear.
-                    scene.backgroundColor =  .clear
-                    view.presentScene(scene)
-                }
-                return view
+        let view = SKView()
+        // We want the view to animate the particle effect over our SwiftUI view
+        // and let its components show through so we'll set allowsTransparenty to true.
+        view.allowsTransparency = true
+        // Load our custom SKScene
+        // More info about creating scene in Xcode is
+        // https://developer.apple.com/documentation/spritekit/skscene/creating_a_scene_from_a_file
+        if let scene = SceneSanjay(fileNamed: "MySceneSanjay.sks") {
+            // We need to set the background to clear.
+            scene.backgroundColor =  .clear
+            view.presentScene(scene)
+        }
+        return view
     }
     
     func updateUIView(_ uiView: SKView, context: UIViewRepresentableContext<EmitterSpriteKitView>) {
@@ -42,17 +42,17 @@ struct EmitterSpriteKitView: UIViewRepresentable {
 }
 
 /**
-This is our SKScene subclass that will present the emitter node.
-*/
+ This is our SKScene subclass that will present the emitter node.
+ */
 class SceneSanjay: SKScene {
     override func didMove(to view: SKView) {
-       super.didMove(to: view)
-
-       // Create our SKEmitterNode with a particle effect named "MyParticleMagic"
-       // More info about creating particle effects in Xcode is
-       // https://help.apple.com/xcode/mac/current/#/dev9eed16018
+        super.didMove(to: view)
         
-       if let emitter: SKEmitterNode = SKEmitterNode(fileNamed: "MyParticleMagic.sks") {
+        // Create our SKEmitterNode with a particle effect named "MyParticleMagic"
+        // More info about creating particle effects in Xcode is
+        // https://help.apple.com/xcode/mac/current/#/dev9eed16018
+        
+        if let emitter: SKEmitterNode = SKEmitterNode(fileNamed: "MyParticleMagic.sks") {
             // Set the initial alpha of the node to 0, as we're going to fade it in.
             emitter.alpha = 0
             // Add the emitter node to the scene
