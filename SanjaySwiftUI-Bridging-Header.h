a26f4aafd31479f2acf42d71ff649b6e490867be
Sanjay Sampat <sanjaysampat@hotmail.com>
2020-11-02 20:55:22 +0530

    Bridging header committed.

---
+++ SanjaySwiftUI-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  SanjaySwiftUI-Bridging-Header.h
+//  SanjaySwiftUI
+//
+//  Created by Sanjay Sampat on 02/11/20.
+//  Copyright © 2020 Sanjay Sampat. All rights reserved.
+//
+
+#ifndef SanjaySwiftUI_Bridging_Header_h
+#define SanjaySwiftUI_Bridging_Header_h
+
+#import "ReachabilityDg.h"
+
+#endif /* SanjaySwiftUI_Bridging_Header_h */
---
+++ SanjaySwiftUIPay-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  SanjaySwiftUIPay-Bridging-Header.h
+//  SanjaySwiftUIPay
+//
+//  Created by Sanjay Sampat on 02/11/20.
+//  Copyright © 2020 Sanjay Sampat. All rights reserved.
+//
+
+#ifndef SanjaySwiftUIPay_Bridging_Header_h
+#define SanjaySwiftUIPay_Bridging_Header_h
+
+#import "ReachabilityDg.h"
+
+#endif /* SanjaySwiftUIPay_Bridging_Header_h */
